dfc4cdfc2c04c2d2b807c8aa3076ce1b35a81591
emilinda1 <emily.carolaine.antunes@escola.pr.gov.br>
2026-06-25 13:21:35 -0300

    11

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function diminuiTamanho(){
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
@@ -22,4 +23,17 @@ function aumentaTamanho(){
     numeroSenha.textContent = tamanhoSenha;
 }
 
-console.log(botoes)
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+
+campoSenha.value = letrasMaiusculas;
+
+function geraSenha(){
+    let senha = '' ”;
+    for (let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+}

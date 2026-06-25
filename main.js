31c55a06f2ff6152a1d2938f14f580590b3203c6
emilinda1 <emily.carolaine.antunes@escola.pr.gov.br>
2026-06-25 12:58:17 -0300

    11

--- main.js
+++ main.js
@@ -5,9 +5,14 @@ numeroSenha.textContent = tamanhoSenha;
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
-    tamanhoSenha = tamanhoSenha-1;
+    if (tamanhoSenha > 1){
+       // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
     numeroSenha.textContent = tamanhoSenha;
 }
+
 console.log(botoes)
